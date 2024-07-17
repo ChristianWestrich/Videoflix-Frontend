ab4d147100f00d151e3e578ce68f977b00cd3019
ChristianWestrich <christian.westrich@gmx.de>
2024-07-17 20:28:21 +0200

    update login, register, email-link

--- src/app/password-confirmation/password-confirmation.component.spec.ts
+++ src/app/password-confirmation/password-confirmation.component.spec.ts
@@ -21,3 +21,4 @@ describe('PasswordConfirmationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/app/password-confirmation/password-confirmation.component.ts
+++ src/app/password-confirmation/password-confirmation.component.ts
@@ -40,3 +40,4 @@ export class PasswordConfirmationComponent {
         return this.passwordIsMatching;
     }
 }
+
